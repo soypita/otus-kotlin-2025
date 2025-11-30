19ebb77cb6593342232b9a8d511289e692b9bf2f
Dmitrii Sazonov <dmitrii.sazonov@deliveryhero.com>
2025-11-30 19:58:36 +0100

    m1l1 first

---
+++ m1l1/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package ru.otus.otuskotlin.marketplace
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Hello world")
+}
